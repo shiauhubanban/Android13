8fb83762c6b2b005a4de451574fd72fc9dd28114
user <a>
2016-11-24 10:18:04 +0800

    v3 Service

--- app/src/main/java/com/org/iii/shine13/MyService.java
+++ app/src/main/java/com/org/iii/shine13/MyService.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
+    private Timer timer;
+    private int i ;
+
     public MyService() {
     }
 
@@ -19,18 +25,31 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.v("shine","onCreate()");
+        timer = new Timer();
+        timer.schedule(new MyTask(),1000,1000);
+        //Log.v("shine","onCreate()");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.v("shine","onStartCommand()");
+        //Log.v("shine","onStartCommand()");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.v("shine","onDestroy()");
+        if(timer !=null){
+            timer.purge(); //清掉
+            timer.cancel();
+            timer = null; //清除記憶體
+        }
+        //Log.v("shine","onDestroy()");
+    }
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            Log.v("shine","i="+i ++);
+        }
     }
 }
